9b121cd62386ccc935f90b89d5eb939a9c6fd02e
Demigod122111 <simonsmithcampion9@gmail.com>
2021-09-23 13:09:03 -0400

    test register

---
+++ user.cs
@@ -0,0 +1,30 @@
+using System;
+using Linq;
+
+public class Users{
+  
+  public List<string> username,password;
+  
+  public static void add(string user, pass){
+    username.Add(user);
+    password.Add(pass)
+  }
+  public static void main(string[] args){
+    
+    check();
+      if(check == true){
+        index.html
+      }
+    }
+    public static bool check(string user, pass){
+      if(username != null && password
+ != null ){
+   if(username.FindIndexOf(user.toString) && password.FindIndexOf(pass.toString)){
+     return true;
+   }else{
+     return false;
+   }
+ }
+    }
+  }
+}
